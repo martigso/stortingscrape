ae88960a829e4951669921f4278af7d35d970a47
Martin Søyland <m.g.soyland@stv.uio.no>
2021-05-28 13:08:10 +0200

    Script for extracting all parties

---
+++ R/get_all_parties.R
@@ -0,0 +1,42 @@
+#' All parliamentary parties
+#' 
+#' A function for retrieving all Norwegian parliamentary parties after WWII
+#' 
+#' @usage get_all_parties()
+#'  
+#' @return A data.frame with response date, version ...
+#' 
+#' @family get_mp_data
+#' 
+#' 
+#' @examples 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' @import rvest
+#' @export
+#' 
+
+
+
+get_all_parties <- function(){
+  
+  url <- "https://data.stortinget.no/eksport/allepartier"
+  
+  tmp <- read_html(url)
+  
+  tmp <- data.frame(response_date = tmp %>% html_nodes("partier_liste > parti > respons_dato_tid") %>% html_text(),
+                    version = tmp %>% html_nodes("partier_liste > parti > versjon") %>% html_text(),
+                    id = tmp %>% html_nodes("partier_liste > parti > id") %>% html_text(),
+                    name = tmp %>% html_nodes("partier_liste > parti > navn") %>% html_text(),
+                    represented_party = tmp %>% html_nodes("partier_liste > parti > representert_parti") %>% html_text(),
+                    sessionid = tmp %>% html_nodes("sesjon_id") %>% html_text(),
+                    period_id = tmp %>% html_nodes("stortingsperiode_id") %>% html_text())
+  
+  return(tmp)
+  
+}
+
+
