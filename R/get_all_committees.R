454208f1f37b513ee7a3961667284b73415b7de6
Martin Søyland <martin.soyland@stv.uio.no>
2021-10-04 19:33:38 +0200

    a minor update to committees script

--- R/get_all_committees.R
+++ R/get_all_committees.R
@@ -1,12 +1,13 @@
 #' Parliamentary committees over all sessions
 #'
-#' @description Imports data on all committee names and ids over all sessions in the data.stortinget.no API.
+#' @description Imports data on all committee names and ids over all sessions
+#' in the data.stortinget.no API.
 #'
 #' @usage get_all_committees()
 #'
 #'
 #' @return A data frame of committees, with the variables:
-#' 
+#'
 #' |                   |                           |
 #' |:------------------|:--------------------------|
 #' | **response_date** | Date of data retrieval    |
@@ -14,9 +15,9 @@
 #' | **id**            | Id of the committee       |
 #' | **name**          | Name of the committee     |
 #' | **sessionid**     | Id of session (empty)     |
-#' 
+#'
 #' @md
-#' 
+#'
 #' @seealso [get_session_committees]
 #'
 #'
@@ -33,16 +34,16 @@ get_all_committees <- function(){
   url <- "https://data.stortinget.no/eksport/allekomiteer"
 
   base <- GET(url)
-  
+
   resp <- http_type(base)
   if(resp != "text/xml") stop(paste0("Response of ", url, " is not text/xml."), call. = FALSE)
-  
+
   status <- http_status(base)
   if(status$category != "Success") stop(paste0("Response of ", url, " returned as '", status$message, "'"), call. = FALSE)
-  
+
   tmp <- read_html(base)
-  
-  
+
+
   tmp <- data.frame(response_date = tmp %>% html_elements("komiteer_liste > komite > respons_dato_tid") %>% html_text(),
                     version = tmp %>% html_elements("komiteer_liste > komite > versjon") %>% html_text(),
                     id = tmp %>% html_elements("komiteer_liste > komite > id") %>% html_text(),
