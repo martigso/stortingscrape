b747fd669244157fa3986ed5af21c2ee36b280f7
Martin Søyland <m.g.soyland@stv.uio.no>
2023-12-01 16:36:28 +0100

    converted get_all_parties() to httr2

--- R/get_all_parties.R
+++ R/get_all_parties.R
@@ -28,7 +28,7 @@
 #' head(parties)
 #' }
 #' 
-#' @import httr rvest
+#' @import httr2 rvest
 #' @export
 get_all_parties <- function(){
   
@@ -67,7 +67,7 @@ get_all_parties <- function(){
   }
   
   tmp <- resp |> 
-    resp_body_html(check_type = F) 
+    resp_body_html(check_type = F, encoding = "utf-8") 
   
   tmp <- data.frame(response_date = tmp %>% html_elements("partier_liste > parti > respons_dato_tid") %>% html_text(),
                     version = tmp %>% html_elements("partier_liste > parti > versjon") %>% html_text(),
